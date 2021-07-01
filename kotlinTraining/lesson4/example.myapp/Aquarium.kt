19259b3788533a182ef344e28be6b64eb5e7d27a
Tejash Datta <tejashdatta@gmail.com>
2021-07-01 14:39:59 +0530

    fix indents

--- kotlinTraining/lesson4/example.myapp/Aquarium.kt
+++ kotlinTraining/lesson4/example.myapp/Aquarium.kt
@@ -29,16 +29,16 @@ open class Aquarium(open var length: Int = 100, open var width: Int = 20, open v
     println("another init")
   }
 
-   constructor(numberOfFish: Int): this() {
-     val tank = numberOfFish * 2000 * 1.1
-     height = (tank / (length * width)).toInt()
-   }
+  constructor(numberOfFish: Int): this() {
+    val tank = numberOfFish * 2000 * 1.1
+    height = (tank / (length * width)).toInt()
+ }
 
   fun printSize() {
     println(shape)
     println("Width: $width cm " +
-        "Length: $length cm " +
-        "Height: $height cm "
+            "Length: $length cm " +
+            "Height: $height cm "
     )
 
     println("Volume: $volume l Water: $water l (${ water / volume * 100.0 }% full)")
@@ -47,10 +47,10 @@ open class Aquarium(open var length: Int = 100, open var width: Int = 20, open v
 
 class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
   override var volume: Int
-  get() = (width /  2 * length / 2 * height / 1000 * PI).toInt()
-  set(value) {
-    height = ((value * 1000 / PI) / (width / 2 * length / 2)).toInt()
-  }
+    get() = (width / 2 * length / 2 * height / 1000 * PI).toInt()
+    set(value) {
+      height = ((value * 1000 / PI) / (width / 2 * length / 2)).toInt()
+    }
 
   override var water = volume * 0.8
   override val shape = "cylinder"
