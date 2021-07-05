1dfa3e171024099a1c0efc8cc078fb1ac02284c1
Tejash Datta <tejashdatta@gmail.com>
2021-07-05 21:34:23 +0530

    fizzbuzz done

---
+++ kotlinTraining/practice/FizzBuzz.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+  FizzBuzz(args[0].toInt()).run()
+}
+
+class FizzBuzz(private val x: Int) {
+  companion object {
+    const val FIZZ = "Fizz"
+    const val FIZZ_DIVISOR = 3
+    const val BUZZ = "Buzz"
+    const val BUZZ_DIVISOR = 5
+  }
+
+  fun run() {
+    for(i in 1..x) {
+      when {
+        i % (FIZZ_DIVISOR * BUZZ_DIVISOR) == 0 -> print(FIZZ + BUZZ)
+        i % FIZZ_DIVISOR == 0 -> print(FIZZ)
+        i % BUZZ_DIVISOR == 0 -> print(BUZZ)
+        else -> print(i)
+      }
+      if (i != x) print(", ")
+    }
+  }
+}
