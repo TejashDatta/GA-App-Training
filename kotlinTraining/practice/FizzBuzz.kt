f43b3436c2277990185686f6929e4f8504fd940e
Tejash Datta <tejashdatta@gmail.com>
2021-07-06 11:51:55 +0530

    rename x

--- kotlinTraining/practice/FizzBuzz.kt
+++ kotlinTraining/practice/FizzBuzz.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
   FizzBuzz(args[0].toInt()).run()
 }
 
-class FizzBuzz(private val x: Int) {
+class FizzBuzz(private val upperLimit: Int) {
   companion object {
     const val FIZZ = "Fizz"
     const val FIZZ_DIVISOR = 3
@@ -11,14 +11,14 @@ class FizzBuzz(private val x: Int) {
   }
 
   fun run() {
-    for(i in 1..x) {
+    for(i in 1..upperLimit) {
       when {
         i % (FIZZ_DIVISOR * BUZZ_DIVISOR) == 0 -> print(FIZZ + BUZZ)
         i % FIZZ_DIVISOR == 0 -> print(FIZZ)
         i % BUZZ_DIVISOR == 0 -> print(BUZZ)
         else -> print(i)
       }
-      if (i != x) print(", ")
+      if (i != upperLimit) print(", ")
     }
   }
 }
