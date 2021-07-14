942fabd8943530c2ddac87ded3adf2106db4240a
Tejash Datta <tejashdatta@gmail.com>
2021-07-14 19:57:11 +0530

    lesson 3.2 done

--- androidKotlinTraining/lesson3/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ androidKotlinTraining/lesson3/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,14 +22,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_over, container, false)
-        return binding.root
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
+    // Inflate the layout for this fragment
+    val binding: FragmentGameOverBinding = DataBindingUtil.inflate(inflater,
+      R.layout.fragment_game_over, container, false)
+
+    binding.tryAgainButton.setOnClickListener { view: View ->
+      view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
     }
+
+    return binding.root
+  }
 }
--- androidKotlinTraining/lesson3/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ androidKotlinTraining/lesson3/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -19,22 +19,28 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-    }
+  private lateinit var drawerLayout: DrawerLayout
 
-    // TODO (01) Create the new TitleFragment
-    // Select File->New->Fragment->Fragment (Blank)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    @Suppress("UNUSED_VARIABLE")
+    val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-    // TODO (02) Clean up the new TitleFragment
-    // In our new TitleFragment
+    drawerLayout = binding.drawerLayout
 
-    // TODO (03) Use DataBindingUtil.inflate to inflate and return the titleFragment in onCreateView
-    // In our new TitleFragment
-    // R.layout.fragment_title
+    val navController = this.findNavController(R.id.myNavHostFragment)
+    NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    NavigationUI.setupWithNavController(binding.navView, navController)
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    val navController = this.findNavController(R.id.myNavHostFragment)
+    return NavigationUI.navigateUp(navController, drawerLayout)
+  }
 }
