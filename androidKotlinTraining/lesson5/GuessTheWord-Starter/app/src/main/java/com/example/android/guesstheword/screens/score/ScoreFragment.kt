7152725657103d356b21732d0eeb7b24814a4360
Tejash Datta <tejashdatta@gmail.com>
2021-07-20 20:02:08 +0530

    lesson 5.1 done

--- androidKotlinTraining/lesson5/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ androidKotlinTraining/lesson5/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,29 +22,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
 
-/**
- * Fragment where the final score is shown, after the game is over
- */
 class ScoreFragment : Fragment() {
+  private lateinit var viewModel: ScoreViewModel
+  private lateinit var viewModelFactory: ScoreViewModelFactory
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
 
-        // Inflate view and obtain an instance of the binding class.
-        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
-                inflater,
-                R.layout.score_fragment,
-                container,
-                false
-        )
+    val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+      inflater,
+      R.layout.score_fragment,
+      container,
+      false
+    )
 
-        return binding.root
-    }
+    viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
+    viewModel = ViewModelProvider(this, viewModelFactory).get(ScoreViewModel::class.java)
+
+    return binding.root
+  }
 }
