5da6f0120fe8e5542c7edecd53f41f25c9348cef
Tejash Datta <tejashdatta@gmail.com>
2021-07-20 14:25:38 +0530

    unify formatting

--- androidKotlinTraining/lesson4/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ androidKotlinTraining/lesson4/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -85,37 +85,31 @@ class MainActivity : AppCompatActivity() {
 
   override fun onStart() {
     super.onStart()
-
     Timber.i("onStart Called")
   }
 
   override fun onResume() {
     super.onResume()
-
     Timber.i("onResume Called")
   }
 
   override fun onPause() {
     super.onPause()
-
     Timber.i("onPause Called")
   }
 
   override fun onStop() {
     super.onStop()
-
     Timber.i("onStop Called")
   }
 
   override fun onDestroy() {
     super.onDestroy()
-
     Timber.i("onDestroy Called")
   }
 
   override fun onRestart() {
     super.onRestart()
-
     Timber.i("onRestart Called")
   }
 
