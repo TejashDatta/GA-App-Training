a85eec9fe1119df6cd887ec425ef2065fc16d281
Tejash Datta <tejashdatta@gmail.com>
2021-07-29 21:54:57 +0530

    implement basic recyclerview and

--- androidKotlinTraining/lesson7/RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ androidKotlinTraining/lesson7/RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -21,6 +21,8 @@ import android.content.res.Resources
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
 //import java.util.concurrent.TimeUnit
@@ -59,16 +61,16 @@ import java.text.SimpleDateFormat
  * Returns a string representing the numeric quality rating.
  */
 fun convertNumericQualityToString(quality: Int, resources: Resources): String {
-    var qualityString = resources.getString(R.string.three_ok)
-    when (quality) {
-        -1 -> qualityString = "--"
-        0 -> qualityString = resources.getString(R.string.zero_very_bad)
-        1 -> qualityString = resources.getString(R.string.one_poor)
-        2 -> qualityString = resources.getString(R.string.two_soso)
-        4 -> qualityString = resources.getString(R.string.four_pretty_good)
-        5 -> qualityString = resources.getString(R.string.five_excellent)
-    }
-    return qualityString
+  var qualityString = resources.getString(R.string.three_ok)
+  when (quality) {
+    -1 -> qualityString = "--"
+    0 -> qualityString = resources.getString(R.string.zero_very_bad)
+    1 -> qualityString = resources.getString(R.string.one_poor)
+    2 -> qualityString = resources.getString(R.string.two_soso)
+    4 -> qualityString = resources.getString(R.string.four_pretty_good)
+    5 -> qualityString = resources.getString(R.string.five_excellent)
+  }
+  return qualityString
 }
 
 
@@ -83,8 +85,8 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
  */
 @SuppressLint("SimpleDateFormat")
 fun convertLongToDateString(systemTime: Long): String {
-    return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
-            .format(systemTime).toString()
+  return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
+    .format(systemTime).toString()
 }
 
 /**
@@ -101,36 +103,37 @@ fun convertLongToDateString(systemTime: Long): String {
  *           See: https://developer.android.com/reference/android/text/Spanned
  */
 fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
-    val sb = StringBuilder()
-    sb.apply {
-        append(resources.getString(R.string.title))
-        nights.forEach {
-            append("<br>")
-            append(resources.getString(R.string.start_time))
-            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
-            if (it.endTimeMilli != it.startTimeMilli) {
-                append(resources.getString(R.string.end_time))
-                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
-                append(resources.getString(R.string.quality))
-                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
-                append(resources.getString(R.string.hours_slept))
-                // Hours
-                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
-                // Minutes
-                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
-                // Seconds
-                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
-            }
-        }
+  val sb = StringBuilder()
+  sb.apply {
+    append(resources.getString(R.string.title))
+    nights.forEach {
+      append("<br>")
+      append(resources.getString(R.string.start_time))
+      append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
+      if (it.endTimeMilli != it.startTimeMilli) {
+        append(resources.getString(R.string.end_time))
+        append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
+        append(resources.getString(R.string.quality))
+        append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
+        append(resources.getString(R.string.hours_slept))
+        // Hours
+        append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
+        // Minutes
+        append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
+        // Seconds
+        append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
+      }
     }
-    // fromHtml is deprecated for target API without a flag, but since our minSDK is 19, we
-    // can't use the newer version, which requires minSDK of 24
-    //https://developer.android.com/reference/android/text/Html#fromHtml(java.lang.String,%20int)
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
-    } else {
-        @Suppress("DEPRECATION")
-        return Html.fromHtml(sb.toString())
-    }
+  }
+  // fromHtml is deprecated for target API without a flag, but since our minSDK is 19, we
+  // can't use the newer version, which requires minSDK of 24
+  //https://developer.android.com/reference/android/text/Html#fromHtml(java.lang.String,%20int)
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+    return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+  } else {
+    @Suppress("DEPRECATION")
+    return Html.fromHtml(sb.toString())
+  }
 }
 
+class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
