ed6b074ae8adf7649c1e7164c385328ea388724e
Tejash Datta <tejashdatta@gmail.com>
2021-08-05 21:15:09 +0530

    multi view holder adapter implemented

--- androidKotlinTraining/lesson7/RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ androidKotlinTraining/lesson7/RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -121,7 +121,7 @@ class SleepTrackerFragment : Fragment() {
 
     sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
       it?.let {
-        adapter.submitList(it)
+        adapter.addHeaderAndSubmitList(it)
       }
     })
 
