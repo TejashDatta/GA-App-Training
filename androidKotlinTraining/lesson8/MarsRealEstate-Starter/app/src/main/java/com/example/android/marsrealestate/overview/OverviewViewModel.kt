0bc621a55f14ed46f1f638da51617c26e7a70f4d
Tejash Datta <tejashdatta@gmail.com>
2021-08-10 17:55:55 +0530

    basic image display done, gridの実装中途

--- androidKotlinTraining/lesson8/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ androidKotlinTraining/lesson8/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
@@ -30,6 +31,10 @@ class OverviewViewModel : ViewModel() {
   val response: LiveData<String>
     get() = _response
 
+  private val _properties = MutableLiveData<List<MarsProperty>>()
+  val properties: LiveData<List<MarsProperty>>
+    get() = _properties
+
   init {
     getMarsRealEstateProperties()
   }
@@ -37,8 +42,8 @@ class OverviewViewModel : ViewModel() {
   private fun getMarsRealEstateProperties() {
     viewModelScope.launch {
       try {
-        val listResult = MarsApi.retrofitService.getProperties()
-        _response.value = "Success: ${listResult.size} Mars properties retrieved"
+        _properties.value = MarsApi.retrofitService.getProperties()
+        _response.value = "Success: Mars properties retrieved"
       }
       catch (e: Exception) {
         _response.value = "Failure: ${e.message}"
