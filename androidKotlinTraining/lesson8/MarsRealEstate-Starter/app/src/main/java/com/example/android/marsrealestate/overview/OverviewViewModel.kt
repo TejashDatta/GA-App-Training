e4df1e571ad14a6ae5969018ffdbb29283549264
Tejash Datta <tejashdatta@gmail.com>
2021-08-10 10:32:29 +0530

    reindent + delete comments 1

--- androidKotlinTraining/lesson8/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ androidKotlinTraining/lesson8/MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,29 +21,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-/**
- * The [ViewModel] that is attached to the [OverviewFragment].
- */
 class OverviewViewModel : ViewModel() {
+  private val _response = MutableLiveData<String>()
+  val response: LiveData<String>
+    get() = _response
 
-    // The internal MutableLiveData String that stores the most recent response
-    private val _response = MutableLiveData<String>()
+  init {
+    getMarsRealEstateProperties()
+  }
 
-    // The external immutable LiveData for the response String
-    val response: LiveData<String>
-        get() = _response
-
-    /**
-     * Call getMarsRealEstateProperties() on init so we can display status immediately.
-     */
-    init {
-        getMarsRealEstateProperties()
-    }
-
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
-    private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
-    }
+  private fun getMarsRealEstateProperties() {
+    _response.value = "Set the Mars API Response here!"
+  }
 }
