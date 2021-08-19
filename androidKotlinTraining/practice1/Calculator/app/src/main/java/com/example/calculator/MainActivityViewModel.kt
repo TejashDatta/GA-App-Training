ee3eeedac4357e8a98931fb99302cf7c28c2dbc8
Tejash Datta <tejashdatta@gmail.com>
2021-08-19 14:26:25 +0530

    corrections

--- androidKotlinTraining/practice1/Calculator/app/src/main/java/com/example/calculator/MainActivityViewModel.kt
+++ androidKotlinTraining/practice1/Calculator/app/src/main/java/com/example/calculator/MainActivityViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
-  private var decimalPointIsSet: Boolean = false
+  private var decimalPointIsSet = false
 
   val operand1 = MutableLiveData<String>()
   val operand2 = MutableLiveData<String>()
@@ -15,28 +15,48 @@ class MainActivityViewModel: ViewModel() {
     reset()
   }
 
-  private fun reset(){
-    operand1.value = ""
-    operand2.value = ""
-    result.value = null
+  private fun beginNewOperandInput() {
     decimalPointIsSet = false
   }
 
+  private fun checkForResult() {
+    if (result.value != null) reset()
+  }
+
+  private fun MutableLiveData<String>.completeDecimalPoint(): MutableLiveData<String> {
+    if (this.value?.last() == '.') this.value += '0'
+    return this
+  }
+
+  private fun MutableLiveData<String>.toFloat(): Float? {
+    if (this.value == "") return null
+    return this.value?.toFloat()
+  }
+
+  fun reset(){
+    operand1.value = ""
+    operand2.value = ""
+    operator.value = null
+    result.value = null
+    beginNewOperandInput()
+  }
+
   fun operandInput(digit: Char) {
-    if (result != null) return reset()
+    checkForResult()
     val operand = if (operator.value == null) operand1 else operand2
     operand.value += digit
   }
 
   fun operatorInput(operatorSymbol: Char) {
-    if (result != null) return reset()
-    if (operator.value != null) return
+    checkForResult()
+    if (operand1.completeDecimalPoint().value == "" || operator.value != null) return
     operator.value = operatorSymbol
-    decimalPointIsSet = false
+    beginNewOperandInput()
   }
 
   fun decimalPointInput() {
-    if(decimalPointIsSet) return
+    checkForResult()
+    if (decimalPointIsSet) return
     val operand = if (operator.value == null) operand1 else operand2
     if (operand.value == "") operand.value = "0"
     operand.value += '.'
@@ -44,9 +64,11 @@ class MainActivityViewModel: ViewModel() {
   }
 
   fun requestResult() {
-    val safeOperand1 = operand1.value?.toFloat() ?: return
-    val safeOperand2 = operand2.value?.toFloat() ?: return
+    checkForResult()
+    val safeOperand1 = operand1.toFloat() ?: return
+    val safeOperand2 = operand2.completeDecimalPoint().toFloat() ?: return
     val safeOperator = operator.value ?: return
+
     result.value = when(safeOperator) {
       '+' -> safeOperand1 + safeOperand2
       '-' -> safeOperand1 - safeOperand2
