1090e6d46d6f644f645eca01cf911d698c02c609
Tejash Datta <tejashdatta@gmail.com>
2021-08-20 16:31:11 +0530

    remove live data from private data

--- androidKotlinTraining/practice1/Calculator/app/src/main/java/com/example/calculator/MainActivityViewModel.kt
+++ androidKotlinTraining/practice1/Calculator/app/src/main/java/com/example/calculator/MainActivityViewModel.kt
@@ -1,31 +1,25 @@
 package com.example.calculator
 
-import android.util.Log
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
   private var decimalPointIsSet = false
 
-  private val operand1 = MutableLiveData<String>()
-  private val operand2 = MutableLiveData<String>()
-  private val operator = MutableLiveData<Char>()
-  private val result = MutableLiveData<Float>()
+  private var operand1: String = ""
+  private var operand2: String = ""
+  private var operator: Char? = null
+  private var result: Float? = null
 
-  val output = MediatorLiveData<String>()
+  val output = MutableLiveData<String>()
 
   init {
     reset()
-    output.addSource(operand1) { updateOutput() }
-    output.addSource(operand2) { updateOutput() }
-    output.addSource(operator) { updateOutput() }
-    output.addSource(result) { updateOutput() }
   }
 
   private fun updateOutput() {
-    val formattedResult = if (result.value == null) "" else "= ${result.value}"
-    output.value = "${operand1.value} ${operator.value ?: ""} ${operand2.value} $formattedResult"
+    val formattedResult = if (result == null) "" else "= ${result}"
+    output.value = "${operand1} ${operator ?: ""} ${operand2} $formattedResult"
   }
 
   private fun beginNewOperandInput() {
@@ -33,67 +27,73 @@ class MainActivityViewModel: ViewModel() {
   }
 
   private fun resetIfCalculationCompleted() {
-    if (result.value != null) reset()
+    if (result != null) reset()
   }
 
-  private fun MutableLiveData<String>.completeDecimalPoint(): MutableLiveData<String> {
-    if (this.value?.lastOrNull() == '.') this.value += '0'
-    return this
-  }
-
-  private fun logDisplay() {
-    Log.d(
-      "MainActivityViewModel",
-      "${operand1.value} ${operator.value ?: ""} ${operand2.value} = ${result.value ?: ""}"
-    )
+  private fun String.completeDecimalPoint(): String {
+    return if (this.lastOrNull() == '.') this + '0' else this
   }
 
   fun reset(){
-    operand1.value = ""
-    operand2.value = ""
-    operator.value = null
-    result.value = null
+    operand1 = ""
+    operand2 = ""
+    operator = null
+    result = null
     beginNewOperandInput()
+
+    updateOutput()
   }
 
   fun operandInput(digit: Char) {
     resetIfCalculationCompleted()
 
-    val operand = if (operator.value == null) operand1 else operand2
-    operand.value += digit
+    if (operator == null) operand1 += digit else operand2 += digit
+
+    updateOutput()
   }
 
   fun operatorInput(operatorSymbol: Char) {
     resetIfCalculationCompleted()
 
-    if (operand1.completeDecimalPoint().value == "" || operator.value != null) return
-    operator.value = operatorSymbol
+    operand1 = operand1.completeDecimalPoint()
+    
+    if (operand1 == "" || operator != null) return
+    operator = operatorSymbol
     beginNewOperandInput()
+
+    updateOutput()
   }
 
   fun decimalPointInput() {
     resetIfCalculationCompleted()
 
     if (decimalPointIsSet) return
-    val operand = if (operator.value == null) operand1 else operand2
-    if (operand.value == "") operand.value = "0"
-    operand.value += '.'
+    var operand = if (operator == null) operand1 else operand2
+    if (operand == "") operand = "0"
+    operand += '.'
+    if (operator == null) operand1 = operand else operand2 = operand
     decimalPointIsSet = true
+
+    updateOutput()
   }
 
   fun requestResult() {
     resetIfCalculationCompleted()
 
-    val safeOperand1 = operand1.value?.toFloatOrNull() ?: return
-    val safeOperand2 = operand2.completeDecimalPoint().value?.toFloatOrNull() ?: return
-    val safeOperator = operator.value ?: return
+    operand2 = operand2.completeDecimalPoint()
 
-    result.value = when(safeOperator) {
+    val safeOperand1 = operand1.toFloatOrNull() ?: return
+    val safeOperand2 = operand2.toFloatOrNull() ?: return
+    val safeOperator = operator ?: return
+
+    result = when(safeOperator) {
       '+' -> safeOperand1 + safeOperand2
       '-' -> safeOperand1 - safeOperand2
       '*' -> safeOperand1 * safeOperand2
       '/' -> safeOperand1 / safeOperand2
       else -> throw Exception("Unknown operator")
     }
+
+    updateOutput()
   }
 }
