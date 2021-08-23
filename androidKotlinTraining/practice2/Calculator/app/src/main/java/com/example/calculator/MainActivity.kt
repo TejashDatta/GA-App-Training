5e4a740d498db5e488733f86b884e28c31c3ac5d
Tejash Datta <tejashdatta@gmail.com>
2021-08-23 20:08:57 +0530

    calculator app files from practice 1

---
+++ androidKotlinTraining/practice2/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.calculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val viewModel =
+      ViewModelProvider(this, MainActivityViewModelFactory()).get(MainActivityViewModel::class.java)
+
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    binding.lifecycleOwner = this
+
+    binding.viewModel = viewModel
+
+    binding.buttonGrid.adapter = ButtonGridAdapter(
+      operandClickListener = { operand ->  viewModel.operandInput(operand) },
+      operatorClickListener = { operator ->  viewModel.operatorInput(operator) },
+      decimalPointClickListener = { viewModel.decimalPointInput() },
+      resultClickListener = { viewModel.requestResult() }
+    )
+  }
+}
