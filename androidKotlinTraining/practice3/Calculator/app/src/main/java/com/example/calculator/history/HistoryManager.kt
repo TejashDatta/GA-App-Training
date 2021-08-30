a14546ea017ae4e3a429ba99579cb84a84235ba0
Tejash Datta <tejashdatta@gmail.com>
2021-08-30 18:33:00 +0530

    snake case to camel case

--- androidKotlinTraining/practice3/Calculator/app/src/main/java/com/example/calculator/history/HistoryManager.kt
+++ androidKotlinTraining/practice3/Calculator/app/src/main/java/com/example/calculator/history/HistoryManager.kt
@@ -7,13 +7,13 @@ object HistoryManager {
   private const val QUEUE_SIZE = 30
   val items = ArrayDeque<String>()
 
-  private fun debug_output() {
+  private fun debugOutput() {
     items.forEach { Log.d("HistoryManager", it) }
   }
 
   fun add(item: String) {
     if (items.size >= QUEUE_SIZE) items.removeFirst()
     items.addLast(item)
-    debug_output()
+    debugOutput()
   }
 }
