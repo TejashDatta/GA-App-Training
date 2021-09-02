3c583d46b11db5c58676046ff4b6d7acc81248a3
Tejash Datta <tejashdatta@gmail.com>
2021-09-02 15:12:09 +0530

    use basic adapter instead of list adapter

--- androidKotlinTraining/practice3/Calculator/app/src/main/java/com/example/calculator/history/HistoryListAdapter.kt
+++ androidKotlinTraining/practice3/Calculator/app/src/main/java/com/example/calculator/history/HistoryListAdapter.kt
@@ -7,29 +7,19 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.calculator.databinding.HistoryListItemBinding
 
-class HistoryListAdapter: ListAdapter<String, HistoryListAdapter.ViewHolder>(HistoryItemDiffCallback()) {
-  class HistoryItemDiffCallback: DiffUtil.ItemCallback<String>() {
-    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-      return oldItem == newItem
-    }
-
-    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
-      return oldItem == newItem
-    }
-  }
-
+class HistoryListAdapter: RecyclerView.Adapter<HistoryListAdapter.ViewHolder>() {
   class ViewHolder(val binding: HistoryListItemBinding): RecyclerView.ViewHolder(binding.root)
 
-  init {
-    submitList(listOf("3 + 5 = 8", "12 * 5 = 60", "1 - 1 = 0", "2 * 0.25 = 0.5"))
-  }
+  var items = listOf("3 + 5 = 8", "12 * 5 = 60", "1 - 1 = 0", "2 * 0.25 = 0.5")
+
+  override fun getItemCount() = items.size
 
   override fun onCreateViewHolder(parent: ViewGroup, _viewType: Int): ViewHolder {
     return ViewHolder(HistoryListItemBinding.inflate(LayoutInflater.from(parent.context)))
   }
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    holder.binding.expression = getItem(position)
+    holder.binding.expression = items[position]
     holder.binding.executePendingBindings()
   }
 }
