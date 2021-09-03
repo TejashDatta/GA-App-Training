b485877b03c0114950ca35172dff02558cfffc50
Tejash Datta <tejashdatta@gmail.com>
2021-09-03 11:17:32 +0530

    reintroduce binding + remove invoke

--- androidKotlinTraining/practice3/Calculator/app/src/main/java/com/example/calculator/history/HistoryListAdapter.kt
+++ androidKotlinTraining/practice3/Calculator/app/src/main/java/com/example/calculator/history/HistoryListAdapter.kt
@@ -2,36 +2,26 @@ package com.example.calculator.history
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.calculator.R
+import com.example.calculator.databinding.HistoryListItemBinding
 
 class HistoryListAdapter(
   private val items: List<String>,
   private val onHistorySelected: (String) -> Unit
 ): RecyclerView.Adapter<HistoryListAdapter.ViewHolder>() {
-  class ViewHolder private constructor(val view: TextView): RecyclerView.ViewHolder(view) {
-    companion object {
-      fun from(parent: ViewGroup): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.history_list_item, parent, false) as TextView
-        return ViewHolder(view)
-      }
-    }
-  }
+  class ViewHolder(val binding: HistoryListItemBinding): RecyclerView.ViewHolder(binding.root)
 
   override fun getItemCount() = items.size
 
   override fun onCreateViewHolder(parent: ViewGroup, _viewType: Int): ViewHolder {
-    return ViewHolder.from(parent)
+    return ViewHolder(HistoryListItemBinding.inflate(LayoutInflater.from(parent.context)))
   }
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
     val history = items[position]
-    holder.view.apply {
+    holder.binding.historyText.apply {
       text = history
-      setOnClickListener { onHistorySelected.invoke(history) }
+      setOnClickListener { onHistorySelected(history) }
     }
   }
 }
-
