35c6f3d87fdf78d680cd0cc71584b5b7d4081861
Tejash Datta <tejashdatta@gmail.com>
2021-09-10 13:13:57 +0530

    use binding and complete layout with click handlrs

--- androidKotlinTraining/practice4/Calculator/app/src/main/java/com/example/calculator/calculator/CalculatorActivity.kt
+++ androidKotlinTraining/practice4/Calculator/app/src/main/java/com/example/calculator/calculator/CalculatorActivity.kt
@@ -2,13 +2,15 @@ package com.example.calculator.calculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.calculator.R
+import com.example.calculator.databinding.CalculatorActBinding
 import com.example.calculator.util.replaceFragmentInActivity
 
 class CalculatorActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.calculator_act)
+    DataBindingUtil.setContentView<CalculatorActBinding>(this, R.layout.calculator_act)
 
     val calculatorFragment =
       supportFragmentManager.findFragmentById(R.id.contentFrame) as CalculatorFragment?
--- androidKotlinTraining/practice4/Calculator/app/src/main/java/com/example/calculator/calculator/CalculatorFragment.kt
+++ androidKotlinTraining/practice4/Calculator/app/src/main/java/com/example/calculator/calculator/CalculatorFragment.kt
@@ -1,20 +1,42 @@
 package com.example.calculator.calculator
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.calculator.R
+import com.example.calculator.databinding.CalculatorFragBinding
 
 class CalculatorFragment : Fragment() {
+  companion object {
+    private const val GRID_COLUMNS = 4
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.calculator_frag, container, false)
+    fun newInstance() = CalculatorFragment()
   }
 
-  companion object {
-    fun newInstance() = CalculatorFragment()
+  private lateinit var binding: CalculatorFragBinding
+
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    binding = CalculatorFragBinding.inflate(inflater)
+
+    binding.clearButton.setOnClickListener { testClickHandler() }
+
+    binding.buttonGrid.adapter = ButtonGridAdapter(
+      operandClickListener = { operand ->  testClickHandler(operand) },
+      operatorClickListener = { operator ->  testClickHandler(operator) },
+      decimalPointClickListener = { testClickHandler() },
+      resultClickListener = { testClickHandler() }
+    )
+
+    binding.buttonGrid.addItemDecoration(
+      GridMarginItemDecoration(GRID_COLUMNS, requireActivity().applicationContext))
+
+    return binding.root
+  }
+
+  private fun testClickHandler(input: Char? = null) {
+    Log.d("CalculatorFragment", input?.toString() ?: "method has no input")
   }
 }
