1a512320d173a118440256564dd20417ee39e1ef
Tejash Datta <tejashdatta@gmail.com>
2021-10-13 16:50:33 +0530

    date converter implemented

--- androidKotlinTraining/practice10/NewsReader/app/src/main/java/com/example/newsreader/MainActivity.kt
+++ androidKotlinTraining/practice10/NewsReader/app/src/main/java/com/example/newsreader/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.newsreader.network.NewsApi
+import com.jakewharton.threetenabp.AndroidThreeTen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -20,11 +21,13 @@ abstract class ScopedAppActivity: AppCompatActivity(), CoroutineScope by MainSco
 class MainActivity : ScopedAppActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    AndroidThreeTen.init(this)
 
     // TODO: change later
     launch {
       val newsChannel = NewsApi.retrofitService.getNewsChannel()
       Log.d("MainActivity", newsChannel.toString())
+      Log.d("MainActivity", newsChannel.newsItems[0].pubDate.toString())
     }
 
     setContentView(R.layout.activity_main)
