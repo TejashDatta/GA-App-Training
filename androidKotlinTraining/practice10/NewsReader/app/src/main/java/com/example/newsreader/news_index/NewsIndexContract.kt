f6a67fef7ab534be8ac2d4232a55ed186dcff0ce
Tejash Datta <tejashdatta@gmail.com>
2021-10-27 17:37:24 +0530

    fix contract

--- androidKotlinTraining/practice10/NewsReader/app/src/main/java/com/example/newsreader/news_index/NewsIndexContract.kt
+++ androidKotlinTraining/practice10/NewsReader/app/src/main/java/com/example/newsreader/news_index/NewsIndexContract.kt
@@ -7,6 +7,7 @@ import com.example.newsreader.data.models.NewsItem
 class NewsIndexContract {
   interface View: BaseView<Presenter> {
     fun setRecyclerViewItems(newsItems: List<NewsItem>)
+    fun openInTab(url: String)
   }
 
   interface Presenter: BasePresenter {
