56b2c55ac6349ccf61090db1ce3f3defc7af550b
Tejash Datta <tejashdatta@gmail.com>
2021-11-11 21:22:30 +0530

    share implemented

--- androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/news_index/OptionsModalBottomSheet.kt
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/news_index/OptionsModalBottomSheet.kt
@@ -1,21 +1,39 @@
 package com.example.newsreader.news_index
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import com.example.newsreader.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class OptionsModalBottomSheet : BottomSheetDialogFragment() {
   companion object {
-//    TODO: take news title and url as argument
-    fun newInstance() = OptionsModalBottomSheet()
+    private const val ARGUMENT_NEWS_ITEM_TITLE = "NEWS_ITEM_TITLE"
+    private const val ARGUMENT_NEWS_ITEM_LINK = "NEWS_ITEM_LINK"
+
+    @JvmStatic fun newInstance(newsItemTitle: String, newsItemLink: String) =
+      OptionsModalBottomSheet().apply {
+        arguments = Bundle().apply {
+          putString(ARGUMENT_NEWS_ITEM_TITLE, newsItemTitle)
+          putString(ARGUMENT_NEWS_ITEM_LINK, newsItemLink)
+        }
+      }
   }
 
+  private lateinit var newsItemTitle: String
+  private lateinit var newsItemLink: String
   private lateinit var shareOptionTextView: TextView
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    arguments?.let {
+      newsItemTitle = it.getString(ARGUMENT_NEWS_ITEM_TITLE)!!
+      newsItemLink = it.getString(ARGUMENT_NEWS_ITEM_LINK)!!
+    }
+  }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -32,7 +50,12 @@ class OptionsModalBottomSheet : BottomSheetDialogFragment() {
   }
 
   private fun shareNews() {
-//    TODO: implement sharing
-    Toast.makeText(context, "Share news", Toast.LENGTH_LONG).show()
+    val shareIntent = Intent.createChooser(Intent().apply {
+      action = Intent.ACTION_SEND
+      type = "text/plain"
+      putExtra(Intent.EXTRA_TEXT, newsItemLink)
+      putExtra(Intent.EXTRA_TITLE, newsItemTitle)
+    }, null)
+    startActivity(shareIntent)
   }
 }
