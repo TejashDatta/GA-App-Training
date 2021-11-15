1bee5ffe3c331fb68a9eae74834515f695dd887f
Tejash Datta <tejashdatta@gmail.com>
2021-11-15 23:40:57 +0530

    toolbar done

--- androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/news_index/NewsIndexActivity.kt
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/news_index/NewsIndexActivity.kt
@@ -1,6 +1,9 @@
 package com.example.newsreader.news_index
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.util.replaceFragmentInActivity
 import com.example.newsreader.R
@@ -13,6 +16,7 @@ class NewsIndexActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_content_frame)
+    setSupportActionBar(findViewById(R.id.toolbar))
 
     val newsIndexFragment =
       supportFragmentManager.findFragmentById(R.id.contentFrame) as NewsIndexFragment?
@@ -22,5 +26,37 @@ class NewsIndexActivity : AppCompatActivity() {
 
     newsIndexPresenter =
       NewsIndexPresenter(newsIndexFragment, NewsItemsRepositoryLocator.repository, SchedulerProvider())
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.toolbar_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+    R.id.action_share -> {
+      shareGoogleNewsHomepageURL()
+      true
+    }
+
+//    TODO: implement navigation to followed items list screen
+//    R.id.action_followed_items -> {
+//      true
+//    }
+
+    else -> super.onOptionsItemSelected(item)
+  }
+
+  private fun shareGoogleNewsHomepageURL() {
+    val googleNewsHomepageURL = "https://news.google.com/topstories?hl=ja&gl=JP&ceid=JP:ja"
+
+    val sendIntent = Intent().apply {
+      action = Intent.ACTION_SEND
+      type = "text/plain"
+      putExtra(Intent.EXTRA_TEXT, googleNewsHomepageURL)
+    }
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+    startActivity(shareIntent)
   }
 }
