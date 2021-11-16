c00be8eb4db0e64b9ff74f9c6184e3bfcf6183f3
Tejash Datta <tejashdatta@gmail.com>
2021-11-16 20:05:30 +0530

    followed news manager implemented

---
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/followed_news/FollowedNewsManager.kt
@@ -0,0 +1,54 @@
+package com.example.newsreader.followed_news
+
+import android.content.SharedPreferences
+import com.example.newsreader.data.models.NewsItem
+import com.squareup.moshi.*
+import org.threeten.bp.ZonedDateTime
+
+class FollowedNewsManager(private val sharedPreferences: SharedPreferences) {
+  companion object {
+    private const val ITEMS_KEY = "followed_news"
+  }
+
+  class ZonedDateTimeAdapter {
+    @ToJson fun toJson(zonedDateTime: ZonedDateTime) = zonedDateTime.toString()
+
+    @FromJson fun fromJson(dateTimeString: String) = ZonedDateTime.parse(dateTimeString)
+  }
+
+  private var jsonAdapter: JsonAdapter<List<NewsItem>> =
+    Moshi.Builder().add(ZonedDateTimeAdapter()).build()
+      .adapter(Types.newParameterizedType(List::class.java, NewsItem::class.java))
+  
+  private var _items = mutableListOf<NewsItem>()
+  val items: List<NewsItem>
+    get() = _items
+
+  init {
+    loadItems()
+  }
+
+  private fun saveItems() {
+    sharedPreferences
+      .edit()
+      .putString(ITEMS_KEY, jsonAdapter.toJson(_items))
+      .apply()
+  }
+
+  private fun loadItems() {
+    val safeJson = sharedPreferences.getString(ITEMS_KEY, null) ?: return
+    jsonAdapter.fromJson(safeJson)?.let {
+      _items = it.toMutableList()
+    }
+  }
+
+  fun add(newsItem: NewsItem) {
+    _items.add(newsItem)
+    saveItems()
+  }
+
+  fun remove(newsItem: NewsItem) {
+    _items.remove(newsItem)
+    saveItems()
+  }
+}
---
+++ androidKotlinTraining/practice11/NewsReader/app/src/test/java/com/example/newsreader/FollowedNewsManagerTest.kt
@@ -0,0 +1,80 @@
+package com.example.newsreader
+
+import android.content.SharedPreferences
+import com.example.newsreader.data.models.NewsItem
+import com.example.newsreader.followed_news.FollowedNewsManager
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+import org.threeten.bp.ZonedDateTime
+
+@RunWith(MockitoJUnitRunner::class)
+class FollowedNewsManagerTest {
+  @Mock private lateinit var sharedPreferences: SharedPreferences
+  @Mock private lateinit var sharedPreferencesEditor: SharedPreferences.Editor
+
+  private lateinit var followedNewsManager: FollowedNewsManager
+
+  private var newsItem =
+    NewsItem("test", "testUrl", ZonedDateTime.parse("2021-11-16T16:31:29.042+05:30[Asia/Calcutta]"), "testSource")
+  private var newsItemListJson =
+    "[{\"link\":\"testUrl\",\"pubDate\":\"2021-11-16T16:31:29.042+05:30[Asia/Calcutta]\",\"source\":\"testSource\",\"title\":\"test\"}]"
+
+  private val ITEMS_KEY = "followed_news"
+
+  @Before fun setupSharedPreferencesMocks() {
+    `when`(sharedPreferences.edit()).thenReturn(sharedPreferencesEditor)
+    `when`(sharedPreferencesEditor.putString(eq(ITEMS_KEY), anyString())).thenReturn(sharedPreferencesEditor)
+  }
+
+  private fun setupEmptyFollowedNewsManager() {
+    `when`(sharedPreferences.getString(ITEMS_KEY, null)).thenReturn(null)
+    followedNewsManager = FollowedNewsManager(sharedPreferences)
+  }
+
+  private fun setupFollowedNewsManagerWithNewsItem() {
+    `when`(sharedPreferences.getString(ITEMS_KEY, null)).thenReturn(newsItemListJson)
+    followedNewsManager = FollowedNewsManager(sharedPreferences)
+  }
+
+  @Test fun initLoadsItemsFromSharedPreferences() {
+    setupFollowedNewsManagerWithNewsItem()
+
+    verify(sharedPreferences).getString(ITEMS_KEY, null)
+    assertEquals(followedNewsManager.items[0], newsItem)
+  }
+
+  @Test fun add_addsToList() {
+    setupEmptyFollowedNewsManager()
+
+    followedNewsManager.add(newsItem)
+    assertEquals(followedNewsManager.items[0], newsItem)
+  }
+
+  @Test fun add_addsToSharedPreferences() {
+    setupEmptyFollowedNewsManager()
+
+    followedNewsManager.add(newsItem)
+    verify(sharedPreferences).edit()
+    verify(sharedPreferencesEditor).putString(ITEMS_KEY, newsItemListJson)
+  }
+
+  @Test fun remove_removesFromList() {
+    setupFollowedNewsManagerWithNewsItem()
+
+    followedNewsManager.remove(newsItem)
+    assertEquals(followedNewsManager.items.size, 0)
+  }
+
+  @Test fun remove_removesFromSharedPreferences() {
+    setupFollowedNewsManagerWithNewsItem()
+
+    followedNewsManager.remove(newsItem)
+    verify(sharedPreferences).edit()
+    verify(sharedPreferencesEditor).putString(ITEMS_KEY, "[]")
+  }
+}
