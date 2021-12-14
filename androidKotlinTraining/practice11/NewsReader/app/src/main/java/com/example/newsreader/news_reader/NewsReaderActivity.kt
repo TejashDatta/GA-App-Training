34aa8d37dd2c7445c21fe4aa6d9d1e815faaadea
Tejash Datta <tejashdatta@gmail.com>
2021-12-15 00:03:20 +0530

    drawer and activity

---
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/news_reader/NewsReaderActivity.kt
@@ -0,0 +1,138 @@
+package com.example.newsreader.news_reader
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.forEach
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.calculator.util.replaceFragmentInActivity
+import com.example.newsreader.R
+import com.example.newsreader.SchedulerProvider
+import com.example.newsreader.data.source.NewsItemsRepositoryFactory
+import com.example.newsreader.followed_news.FollowedNewsFragment
+import com.example.newsreader.followed_news.FollowedNewsPresenter
+import com.example.newsreader.news_index.NewsIndexFragment
+import com.example.newsreader.news_index.NewsIndexPresenter
+import com.example.newsreader.recent_news.RecentNewsFragment
+import com.example.newsreader.recent_news.RecentNewsPresenter
+import com.google.android.material.navigation.NavigationView
+
+class NewsReaderActivity : AppCompatActivity(), NewsReaderContract.View {
+  override lateinit var presenter: NewsReaderContract.Presenter
+
+  private lateinit var drawerLayout: DrawerLayout
+  private lateinit var navigationView: NavigationView
+
+  private var newsIndexFragment: NewsIndexFragment? = null
+  private var newsIndexPresenter: NewsIndexPresenter? = null
+
+  private var followedNewsFragment: FollowedNewsFragment? = null
+  private var followedNewsPresenter: FollowedNewsPresenter? = null
+
+  private var recentNewsFragment: RecentNewsFragment? = null
+  private var recentNewsPresenter: RecentNewsPresenter? = null
+
+  override fun onResume() {
+    super.onResume()
+    presenter.start()
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_news_reader)
+
+    setSupportActionBar(findViewById(R.id.toolbar))
+    supportActionBar?.apply {
+      setDisplayShowHomeEnabled(true)
+      setDisplayHomeAsUpEnabled(true)
+      setHomeAsUpIndicator(R.drawable.ic_menu)
+    }
+
+    drawerLayout = findViewById(R.id.drawer_layout)
+    navigationView = findViewById(R.id.navigation_view)
+    setupDrawerContent()
+
+    presenter = NewsReaderPresenter(this)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (item.itemId == android.R.id.home) {
+      drawerLayout.openDrawer(GravityCompat.START)
+      return true
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  override fun showAllNews() {
+    if(supportFragmentManager.findFragmentById(R.id.contentFrame) as? NewsIndexFragment != null)  return
+
+    if (newsIndexFragment == null) newsIndexFragment = NewsIndexFragment.newInstance()
+    replaceFragmentInActivity(R.id.contentFrame, newsIndexFragment!!)
+
+    if (newsIndexPresenter == null) {
+      newsIndexPresenter = NewsIndexPresenter(
+        newsIndexFragment!!,
+        NewsItemsRepositoryFactory.getOrCreateRepository(applicationContext),
+        SchedulerProvider()
+      )
+    }
+  }
+
+  override fun showGoogleNews() {
+    TODO("Not yet implemented")
+  }
+
+  override fun showToyokeizaiNews() {
+    TODO("Not yet implemented")
+  }
+
+  override fun showFollowedNews() {
+    if(supportFragmentManager.findFragmentById(R.id.contentFrame) as? FollowedNewsFragment != null)  return
+
+    if (followedNewsFragment == null) followedNewsFragment = FollowedNewsFragment.newInstance()
+    replaceFragmentInActivity(R.id.contentFrame, followedNewsFragment!!)
+
+    if (followedNewsPresenter == null) {
+      followedNewsPresenter = FollowedNewsPresenter(
+        followedNewsFragment!!,
+        NewsItemsRepositoryFactory.getOrCreateRepository(applicationContext),
+        SchedulerProvider()
+      )
+    }
+  }
+
+  override fun showRecentNews() {
+    if(supportFragmentManager.findFragmentById(R.id.contentFrame) as? RecentNewsFragment != null)  return
+
+    if (recentNewsFragment == null) recentNewsFragment = RecentNewsFragment.newInstance()
+    replaceFragmentInActivity(R.id.contentFrame, recentNewsFragment!!)
+
+    if (recentNewsPresenter == null) {
+      recentNewsPresenter = RecentNewsPresenter(
+        recentNewsFragment!!,
+        NewsItemsRepositoryFactory.getOrCreateRepository(applicationContext)
+      )
+    }
+  }
+
+  private fun setupDrawerContent() {
+    navigationView.setNavigationItemSelectedListener { menuItem ->
+      uncheckMenuItems()
+
+      when(menuItem.itemId) {
+        R.id.action_all_news -> presenter.onClickAllNews()
+        R.id.action_followed_news -> presenter.onClickFollowedNews()
+        R.id.action_recent_news -> presenter.onClickRecentNews()
+      }
+
+      menuItem.isChecked = true
+      drawerLayout.closeDrawers()
+      true
+    }
+  }
+
+  private fun uncheckMenuItems() {
+    navigationView.menu.forEach { it.isChecked = false }
+  }
+}
---
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/news_reader/NewsReaderPresenter.kt
@@ -0,0 +1,29 @@
+package com.example.newsreader.news_reader
+
+class NewsReaderPresenter(
+  private val newsReaderView: NewsReaderContract.View
+): NewsReaderContract.Presenter {
+  override fun start() {
+    newsReaderView.showAllNews()
+  }
+
+  override fun onClickAllNews() {
+    newsReaderView.showAllNews()
+  }
+
+  override fun onClickGoogleNews() {
+    newsReaderView.showGoogleNews()
+  }
+
+  override fun onClickToyokeizaiNews() {
+    newsReaderView.showToyokeizaiNews()
+  }
+
+  override fun onClickFollowedNews() {
+    newsReaderView.showFollowedNews()
+  }
+
+  override fun onClickRecentNews() {
+    newsReaderView.showRecentNews()
+  }
+}
