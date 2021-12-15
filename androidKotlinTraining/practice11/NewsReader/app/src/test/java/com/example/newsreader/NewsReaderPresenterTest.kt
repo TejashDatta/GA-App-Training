5cbbf2b0011520b4efdcf956319bd427c9ac8e8b
Tejash Datta <tejashdatta@gmail.com>
2021-12-15 12:14:59 +0530

    news reader presenter tests

---
+++ androidKotlinTraining/practice11/NewsReader/app/src/test/java/com/example/newsreader/NewsReaderPresenterTest.kt
@@ -0,0 +1,57 @@
+package com.example.newsreader
+
+import com.example.newsreader.news_reader.NewsReaderContract
+import com.example.newsreader.news_reader.NewsReaderPresenter
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class NewsReaderPresenterTest {
+  @Mock private lateinit var newsReaderView: NewsReaderContract.View
+
+  private lateinit var newsReaderPresenter: NewsReaderPresenter
+
+  @Before fun setupNewsIndexPresenter() {
+    newsReaderPresenter = NewsReaderPresenter(newsReaderView)
+  }
+
+  @Test fun start_showsAllNews() {
+    newsReaderPresenter.start()
+
+    verify(newsReaderView).showAllNews()
+  }
+
+  @Test fun onClickAllNews_showsAllNews() {
+    newsReaderPresenter.onClickAllNews()
+
+    verify(newsReaderView).showAllNews()
+  }
+
+  @Test fun onClickAllNews_showsGoogleNews() {
+    newsReaderPresenter.onClickGoogleNews()
+
+    verify(newsReaderView).showGoogleNews()
+  }
+
+  @Test fun onClickAllNews_showsToyokeizaiNews() {
+    newsReaderPresenter.onClickToyokeizaiNews()
+
+    verify(newsReaderView).showToyokeizaiNews()
+  }
+
+  @Test fun onClickAllNews_showsFollowedNews() {
+    newsReaderPresenter.onClickFollowedNews()
+
+    verify(newsReaderView).showFollowedNews()
+  }
+
+  @Test fun onClickAllNews_showsRecentNews() {
+    newsReaderPresenter.onClickRecentNews()
+
+    verify(newsReaderView).showRecentNews()
+  }
+}
