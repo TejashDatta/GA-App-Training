7775f19dba195a39da3fa69a87dcec21b4437cf8
Tejash Datta <tejashdatta@gmail.com>
2021-12-25 19:21:43 +0530

    implement presenter validation

--- androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/add_news_source/AddNewsSourcePresenter.kt
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/add_news_source/AddNewsSourcePresenter.kt
@@ -6,6 +6,13 @@ class AddNewsSourcePresenter(
   private val view: AddNewsSourceContract.View,
   private val newsItemsRepository: NewsItemsRepository
 ): AddNewsSourceContract.Presenter {
+  companion object {
+    const val NAME_MAX_LENGTH = 30
+    const val URL_MAX_LENGTH  = 200
+  }
+
+//  TODO: check regex pattern
+  private val urlRegex = Regex("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
 
   init {
     view.presenter = this
@@ -16,11 +23,20 @@ class AddNewsSourcePresenter(
   }
 
   override fun validateName(name: String) {
-    TODO("Not yet implemented")
+    when {
+      name.isEmpty() -> view.setNameIsRequiredError()
+      name.length > NAME_MAX_LENGTH -> view.setNameIsTooLongError(NAME_MAX_LENGTH)
+      else -> view.disableNameError()
+    }
   }
 
   override fun validateUrl(url: String) {
-    TODO("Not yet implemented")
+    when {
+      url.isEmpty() -> view.setUrlIsRequiredError()
+      url.length > URL_MAX_LENGTH -> view.setUrlIsTooLongError(URL_MAX_LENGTH)
+      !url.matches(urlRegex) -> view.setUrlFormatError()
+      else -> view.disableUrlError()
+    }
   }
 
   override fun onSaveClick(name: String, url: String) {
