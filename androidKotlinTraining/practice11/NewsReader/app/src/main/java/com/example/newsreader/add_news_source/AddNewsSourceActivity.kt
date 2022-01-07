9f90ccada9c44e8ec61890fc157dfdca8265ac79
Tejash Datta <tejashdatta@gmail.com>
2022-01-07 19:32:49 +0530

    use save click listener

--- androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/add_news_source/AddNewsSourceActivity.kt
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/add_news_source/AddNewsSourceActivity.kt
@@ -12,6 +12,7 @@ import com.example.newsreader.data.validators.NewsSourceValidator
 class AddNewsSourceActivity : AppCompatActivity() {
   private lateinit var addNewsSourceFragment: AddNewsSourceFragment
   private lateinit var addNewsSourcePresenter: AddNewsSourcePresenter
+  private lateinit var saveClickListener: SaveClickListener
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -20,6 +21,7 @@ class AddNewsSourceActivity : AppCompatActivity() {
     setSupportActionBar(findViewById(R.id.toolbar))
 
     addNewsSourceFragment = AddNewsSourceFragment.newInstance()
+    saveClickListener = addNewsSourceFragment
 
     supportFragmentManager.beginTransaction().apply {
       add(R.id.contentFrame, addNewsSourceFragment)
@@ -43,7 +45,7 @@ class AddNewsSourceActivity : AppCompatActivity() {
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
     return when (item.itemId) {
       R.id.action_save -> {
-        addNewsSourceFragment.onSaveClick()
+        saveClickListener.onSaveClick()
         true
       }
       else -> super.onOptionsItemSelected(item)
