db41d0d80ae435aad9b3d784bcb504a500eb01c9
Tejash Datta <tejashdatta@gmail.com>
2022-01-18 11:23:08 +0530

    fix existing test names

--- androidKotlinTraining/practice11/NewsReader/app/src/test/java/com/example/newsreader/NewsReaderPresenterTest.kt
+++ androidKotlinTraining/practice11/NewsReader/app/src/test/java/com/example/newsreader/NewsReaderPresenterTest.kt
@@ -31,25 +31,25 @@ class NewsReaderPresenterTest {
     verify(newsReaderView).showAllNews()
   }
 
-  @Test fun onClickAllNews_showsGoogleNews() {
+  @Test fun onClickGoogleNews_showsGoogleNews() {
     newsReaderPresenter.onClickGoogleNews()
 
     verify(newsReaderView).showGoogleNews()
   }
 
-  @Test fun onClickAllNews_showsToyokeizaiNews() {
+  @Test fun onClickToyokeizaiNews_showsToyokeizaiNews() {
     newsReaderPresenter.onClickToyokeizaiNews()
 
     verify(newsReaderView).showToyokeizaiNews()
   }
 
-  @Test fun onClickAllNews_showsFollowedNews() {
+  @Test fun onClickFollowedNews_showsFollowedNews() {
     newsReaderPresenter.onClickFollowedNews()
 
     verify(newsReaderView).showFollowedNews()
   }
 
-  @Test fun onClickAllNews_showsRecentNews() {
+  @Test fun onClickRecentNews_showsRecentNews() {
     newsReaderPresenter.onClickRecentNews()
 
     verify(newsReaderView).showRecentNews()
