ac4992f850c1b1b7e5ef9b6ceb3467ee27bb2442
Tejash Datta <tejashdatta@gmail.com>
2022-01-26 22:23:22 +0530

    use new api in repository

--- androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/data/source/NewsRepository.kt
+++ androidKotlinTraining/practice11/NewsReader/app/src/main/java/com/example/newsreader/data/source/NewsRepository.kt
@@ -4,12 +4,8 @@ import android.app.Application
 import android.content.Context
 import com.example.newsreader.data.models.NewsItem
 import com.example.newsreader.data.models.NewsSource
-import com.example.newsreader.network.GeneralNewsApi
-import com.example.newsreader.network.GoogleNewsApi
-import com.example.newsreader.network.ToyokeizaiNewsApi
-import com.example.newsreader.network.data_transfer_objects.general_news.toDomainModel
-import com.example.newsreader.network.data_transfer_objects.google_news.toDomainModel
-import com.example.newsreader.network.data_transfer_objects.toyokeizai_news.toDomainModel
+import com.example.newsreader.network.NewsApi
+import com.example.newsreader.network.data_transfer_objects.news.toDomainModel
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 
@@ -33,9 +29,7 @@ object NewsRepositoryFactory: Application() {
       context.getSharedPreferences(NEWS_SOURCES_SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)
 
     return NewsRepository(
-      GoogleNewsApi,
-      ToyokeizaiNewsApi,
-      GeneralNewsApi,
+      NewsApi,
       FollowedNewsManager(followedNewsSharedPreferences),
       RecentNewsManager(recentNewsSharedPreferences),
       NewsSourcesManager(newsSourcesSharedPreferences)
@@ -46,45 +40,17 @@ object NewsRepositoryFactory: Application() {
 }
 
 class NewsRepository(
-  private val googleNewsApi: GoogleNewsApi,
-  private val toyokeizaiNewsApi: ToyokeizaiNewsApi,
-  private val generalNewsApi: GeneralNewsApi,
+  private val newsApi: NewsApi,
   private val followedNewsManager: FollowedNewsManager,
   private val recentNewsManager: RecentNewsManager,
   private val newsSourcesManager: NewsSourcesManager
 ) {
 
-  private var cachedGoogleNews: List<NewsItem>? = null
-  private var cachedToyokeizaiNews: List<NewsItem>? = null
   private var generalNewsCache = HashMap<String, List<NewsItem>>()
 
-  private fun requestGoogleNews(): Observable<List<NewsItem>> {
-    return googleNewsApi.retrofitService.getNewsChannel().map { it.toDomainModel() }
-  }
-
-  private fun requestToyokeizaiNews(): Observable<List<NewsItem>> {
-    return toyokeizaiNewsApi.retrofitService.getNewsChannel().map { it.toDomainModel() }
-  }
-
   private fun requestGeneralNews(newsSource: NewsSource): Observable<List<NewsItem>> {
-    return generalNewsApi.retrofitService
+    return newsApi.retrofitService
              .getNewsChannel(newsSource.url).map { it.toDomainModel(newsSource.name) }
-  }
-
-  private fun getGoogleNews(refresh: Boolean): Observable<List<NewsItem>> {
-    return if (refresh || cachedGoogleNews == null) {
-      requestGoogleNews().doOnNext { cachedGoogleNews = it }
-    } else {
-      Observable.just(cachedGoogleNews!!)
-    }
-  }
-
-  private fun getToyokeizaiNews(refresh: Boolean): Observable<List<NewsItem>> {
-    return if (refresh || cachedToyokeizaiNews == null) {
-      requestToyokeizaiNews().doOnNext { cachedToyokeizaiNews = it }
-    } else {
-      Observable.just(cachedToyokeizaiNews!!)
-    }
   }
 
   private fun getGeneralNews(newsSource: NewsSource, refresh: Boolean): Observable<List<NewsItem>> {
@@ -97,8 +63,6 @@ class NewsRepository(
 
   private fun getAllNews(refresh: Boolean): Observable<List<NewsItem>> {
     val getNewsObservables = mutableListOf<Observable<List<NewsItem>>>()
-    getNewsObservables.add(getGoogleNews(refresh))
-    getNewsObservables.add(getToyokeizaiNews(refresh))
     newsSourcesSubject.value?.let { newsSources ->
       newsSources.forEach { getNewsObservables.add(getGeneralNews(it, refresh)) }
     }
@@ -118,10 +82,8 @@ class NewsRepository(
   }
 
   fun getNews(newsSource: NewsSource, refresh: Boolean): Observable<List<NewsItem>> {
-    return when(newsSource) {
-      stockNewsSources.all -> getAllNews(refresh)
-      stockNewsSources.google -> getGoogleNews(refresh)
-      stockNewsSources.toyokeizai -> getToyokeizaiNews(refresh)
+    return when(newsSource.name) {
+      NewsSourcesManager.ALL_NEWS_NAME -> getAllNews(refresh)
       else -> getGeneralNews(newsSource, refresh)
     }
   }
@@ -139,8 +101,6 @@ class NewsRepository(
     get() = recentNewsManager.items
 
   fun addRecentNews(newsItem: NewsItem) = recentNewsManager.add(newsItem)
-
-  val stockNewsSources = StockNewsSources()
 
   val newsSourcesSubject: BehaviorSubject<List<NewsSource>>
     get() = newsSourcesManager.newsSourcesSubject
