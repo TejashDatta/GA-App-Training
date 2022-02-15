c23b9587a4cb2c8e8e045075df686f83173ea64a
Tejash Datta <tejashdatta@gmail.com>
2022-02-15 12:48:30 +0530

    fix routing

--- railsTraining/practice1/config/routes.rb
+++ railsTraining/practice1/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-  resources :articles, path: '/'
-
   get '/admin-login', to: 'admin_authentication#new', as: :admin_login
   post '/admin-login', to: 'admin_authentication#create'
   delete '/admin-logout', to: 'admin_authentication#destroy', as: :admin_logout
+
+  root 'articles#index'
+  resources :articles, path: '/'
 end
