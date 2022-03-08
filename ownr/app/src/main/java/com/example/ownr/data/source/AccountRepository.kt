1dce6ea4111715aca46591435a62832695ea0934
Tejash Datta <tejashdatta@gmail.com>
2022-03-09 01:12:56 +0530

    remove factory from repo

--- ownr/app/src/main/java/com/example/ownr/data/source/AccountRepository.kt
+++ ownr/app/src/main/java/com/example/ownr/data/source/AccountRepository.kt
@@ -1,24 +1,6 @@
 package com.example.ownr.data.source
 
-import android.app.Application
-import android.content.Context
 import android.content.SharedPreferences
-
-object AccountRepositoryFactory: Application() {
-  private const val CURRENT_USER_PREFERENCES_KEY = "CURRENT_USER_PREFERENCES"
-
-  private var repository : AccountRepository? = null
-
-  fun getInstance(context: Context) = repository ?: initRepository(context)
-
-  private fun initRepository(context: Context): AccountRepository  {
-    return AccountRepository(
-      context.getSharedPreferences(CURRENT_USER_PREFERENCES_KEY, Context.MODE_PRIVATE)
-    ).also {
-      repository = it
-    }
-  }
-}
 
 class AccountRepository(private val currentUserSharedPreferences: SharedPreferences) {
   companion object {
