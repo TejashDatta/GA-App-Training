9bfb7cc627203f40cf536e8b8b898f8cd038694c
Tejash Datta <tejashdatta@gmail.com>
2022-03-10 16:34:13 +0530

    make appComponent accessible

--- ownr/app/src/main/java/com/example/ownr/OwnrApp.kt
+++ ownr/app/src/main/java/com/example/ownr/OwnrApp.kt
@@ -1,16 +1,14 @@
 package com.example.ownr
 
 import android.app.Application
-import android.content.Context
+import com.example.ownr.di.AppComponent
 import com.example.ownr.di.DaggerAppComponent
-import dagger.Component
-import dagger.Module
-import dagger.Provides
-import javax.inject.Singleton
 
 class OwnrApp: Application() {
+  lateinit var appComponent: AppComponent
+
   override fun onCreate() {
     super.onCreate()
-    val appComponent = DaggerAppComponent.factory().create(applicationContext)
+    appComponent = DaggerAppComponent.factory().create(applicationContext)
   }
 }
