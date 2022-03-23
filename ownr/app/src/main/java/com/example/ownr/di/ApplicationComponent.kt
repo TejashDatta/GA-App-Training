9324d8da2978af1d44786a5334ab48cfc3d11d49
Tejash Datta <tejashdatta@gmail.com>
2022-03-23 13:41:07 +0530

    remove inject

--- ownr/app/src/main/java/com/example/ownr/di/ApplicationComponent.kt
+++ ownr/app/src/main/java/com/example/ownr/di/ApplicationComponent.kt
@@ -2,7 +2,6 @@ package com.example.ownr.di
 
 import android.content.Context
 import com.example.ownr.OwnrApplication
-import com.example.ownr.splash.SplashActivity
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
@@ -21,6 +20,4 @@ interface ApplicationComponent: AndroidInjector<OwnrApplication> {
   interface Factory {
     fun create(@BindsInstance applicationContext: Context): ApplicationComponent
   }
-
-  fun inject(activity: SplashActivity)
 }
