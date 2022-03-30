7bfebcc480e64332a13feb8fe4eeaa050888b16b
Tejash Datta <tejashdatta@gmail.com>
2022-03-30 23:04:56 +0530

    inject scheduler provider

--- ownr/app/src/main/java/com/example/ownr/di/ApplicationComponent.kt
+++ ownr/app/src/main/java/com/example/ownr/di/ApplicationComponent.kt
@@ -13,6 +13,7 @@ import javax.inject.Singleton
   modules = [
     AndroidSupportInjectionModule::class,
     ActivityBindingModule::class,
+    SchedulerProviderModule::class,
     AccountRepositoryModule::class
   ])
 interface ApplicationComponent: AndroidInjector<OwnrApplication> {
