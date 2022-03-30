81685e06790316943374b09fc52cf85c19077544
Tejash Datta <tejashdatta@gmail.com>
2022-03-30 23:06:32 +0530

    used scheduler in presenter and test

--- ownr/app/src/main/java/com/example/ownr/splash/SplashPresenter.kt
+++ ownr/app/src/main/java/com/example/ownr/splash/SplashPresenter.kt
@@ -1,12 +1,14 @@
 package com.example.ownr.splash
 
+import com.example.ownr.BaseSchedulerProvider
 import com.example.ownr.data.source.AccountRepository
 import io.reactivex.rxjava3.core.Observable
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class SplashPresenter @Inject constructor(
-  private val accountRepository: AccountRepository
+  private val accountRepository: AccountRepository,
+  private val schedulerProvider: BaseSchedulerProvider
 ): SplashContract.Presenter {
   companion object {
     const val SPLASH_DISPLAY_SECONDS = 2
@@ -16,7 +18,9 @@ class SplashPresenter @Inject constructor(
 
   override fun takeView(view: SplashContract.View) {
     this.view = view
-    Observable.timer(SPLASH_DISPLAY_SECONDS.toLong(), TimeUnit.SECONDS).subscribe {
+    Observable.timer(
+      SPLASH_DISPLAY_SECONDS.toLong(), TimeUnit.SECONDS, schedulerProvider.computation()
+    ).subscribe {
       changeActivity()
     }
   }
--- ownr/app/src/test/java/com/example/ownr/data/source/splash/SplashPresenterTest.kt
+++ ownr/app/src/test/java/com/example/ownr/data/source/splash/SplashPresenterTest.kt
@@ -1,9 +1,11 @@
 package com.example.ownr.data.source.splash
 
 import com.example.ownr.data.source.AccountRepository
+import com.example.ownr.data.source.TestSchedulerProvider
 import com.example.ownr.splash.SplashContract
 import com.example.ownr.splash.SplashPresenter
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.TestScheduler
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,27 +13,33 @@ import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.concurrent.TimeUnit
 
 @RunWith(MockitoJUnitRunner::class)
 class SplashPresenterTest {
   private lateinit var splashPresenter: SplashPresenter
 
+  private val testScheduler = TestScheduler()
+  private val testSchedulerProvider = TestSchedulerProvider(testScheduler)
+
   @Mock private lateinit var accountRepository: AccountRepository
   @Mock private lateinit var splashView: SplashContract.View
 
   @Before fun setupPresenter() {
-    splashPresenter = SplashPresenter(accountRepository)
+    splashPresenter = SplashPresenter(accountRepository, testSchedulerProvider)
   }
 
   @Test fun takeViewChangesActivityToLoginWhenNotLoggedIn() {
     `when`(accountRepository.isLoggedIn()).thenReturn(Observable.just(false))
     splashPresenter.takeView(splashView)
+    testScheduler.advanceTimeBy(SplashPresenter.SPLASH_DISPLAY_SECONDS.toLong(), TimeUnit.SECONDS)
     verify(splashView).showLogin()
   }
 
   @Test fun takeViewChangesActivityToPropertyListWhenLoggedIn() {
     `when`(accountRepository.isLoggedIn()).thenReturn(Observable.just(true))
     splashPresenter.takeView(splashView)
+    testScheduler.advanceTimeBy(SplashPresenter.SPLASH_DISPLAY_SECONDS.toLong(), TimeUnit.SECONDS)
     verify(splashView).showPropertyList()
   }
 }
